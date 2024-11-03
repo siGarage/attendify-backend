14465952870df946d75c70208b43d471d319dd5e
kartik <kartik65361@gmail.com>
2024-11-04 01:44:32 +0530

    biometric

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -5,7 +5,7 @@ import StudentAttendance from "../models/studentAttendenceModel.js";
 // import LASTUPDATE from "../models/lastUpdateAttendanceModel.js";
 import SEMESTER from "../models/semesterModel.js";
 import Validator from "validatorjs";
-import BIOMETRIC from "../models/biometricModel.js";
+import Biometric from "../models/biometricModel.js";
 import USER from "../models/userModel.js";
 import TEACHER from "../models/teacherModel.js";
 import reply from "../common/reply.js";
@@ -169,24 +169,25 @@ export default {
         finger_id_1: "required",
         finger_id_2: "required",
         finger_id_3: "required",
+        face: null,
       });
       if (validation.fails()) {
         let err_key = Object.keys(Object.entries(validation.errors)[0][1])[0];
         return res.json(reply.failed(validation.errors.first(err_key)));
       }
-      let exist = await BIOMETRIC.findOne({
-        user_id: request.user_id,
+      let exist = await Biometric.findOne({
+        user_id: request.user_id
       });
       if (exist) {
         let _id = req.body.id;
-        await BIOMETRIC.findByIdAndUpdate(_id, request);
+        await Biometric.findByIdAndUpdate(_id, request);
         return res
           .status(201)
           .send({ message: "Biometric updated successfully" });
       }
-      let Biometric = await BIOMETRIC.create(request);
+      let BiometricData = await Biometric.create(request);
       return res.status(200).send({
-        Biometric: Biometric,
+        BiometricData: BiometricData,
         message: "Biometric created successfully",
       });
     } catch (err) {
