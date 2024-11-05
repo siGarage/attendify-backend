a3f6d0639c3c05d90a32e0ee3ae4508885e2c6eb
Rashid Mohamad <rashid2538@gmail.com>
2024-11-05 20:12:51 +0530

    Changed response to always 200 and removed unnecessary data from response.

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -169,29 +169,26 @@ export default {
         finger_id_1: "required",
         finger_id_2: "required",
         finger_id_3: "required",
-        face: null,
       });
       if (validation.fails()) {
         let err_key = Object.keys(Object.entries(validation.errors)[0][1])[0];
         return res.json(reply.failed(validation.errors.first(err_key)));
       }
       let exist = await Biometric.findOne({
-        user_id: request.user_id
+        user_id: request.user_id,
+        type: request.type,
       });
       if (exist) {
         let _id = req.body.id;
         await Biometric.findByIdAndUpdate(_id, request);
-        return res
-          .status(201)
-          .send({ message: "Biometric updated successfully" });
+        return res.json({ message: "Biometric updated successfully" });
       }
-      let BiometricData = await Biometric.create(request);
+      await Biometric.create(request);
       return res.status(200).send({
-        BiometricData: BiometricData,
         message: "Biometric created successfully",
       });
     } catch (err) {
-      return res.status(500).send({ message: "Internal Server Error" });
+      return res.status(200).send({ message: "Internal Server Error: " + err.message });
     }
   },
   async getTeacherDetails(req, res) {
