c82286684f3d5eb12a1becc0140c1d1146681007
kartik <kartik65361@gmail.com>
2024-11-11 21:37:37 +0530

    some changes

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -102,27 +102,31 @@ export default {
       }
       let _id = req.body.id;
       const teacher = await Teacher.findById(_id);
+      const teacherIds = req.body.map(
+        (d) => d.department_id == req.body.department_id
+      );
+      console.log(teacherIds);
       if (!teacher) {
         return res.status(404).send({ message: "Teacher not found" });
       }
       if (req.body.role == "2") {
-        const test=await User.findOneAndUpdate(
+        const test = await User.findOneAndUpdate(
           { role: "2", _id: req.body.user_id },
           { $set: { role: "3" } },
           { new: true }
         );
-        console.log(test,'kartik1');
+        console.log(test, "kartik1");
         await Teacher.findByIdAndUpdate(_id, request);
         return res
           .status(201)
           .send({ teacher: request, message: "Teacher updated successfully" });
       } else {
-        const teast2=await User.findOneAndUpdate(
+        const teast2 = await User.findOneAndUpdate(
           { _id: req.body.user_id },
           { $set: { role: "2" } },
           { new: true }
         );
-        console.log(teast2,'kartik2');
+        console.log(teast2, "kartik2");
         await Teacher.findByIdAndUpdate(_id, request);
         return res
           .status(201)
